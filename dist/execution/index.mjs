cf7ab317116e07cf319163556d129d6709c60842
Andreas <andreaslundquist1@gmail.com>
2020-03-21 16:47:53 +0100

    Remove dist folder

--- dist/execution/index.mjs
+++
@@ -1,3 +0,0 @@
-export { pathToArray as responsePathAsArray } from "../jsutils/Path.mjs";
-export { execute, defaultFieldResolver, defaultTypeResolver, buildExecutionContext, buildResolveInfo } from "./execute.mjs";
-export { getDirectiveValues, getArgumentValues } from "./values.mjs";
